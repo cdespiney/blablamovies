fa388a4f35ae811ec561e5524f2619637474107d
Clément Despiney <clement.despiney@epitech.eu>
2021-03-07 13:23:14 +0100

    feat: added paginated fetching

--- src/Components/Selection/Selection.js
+++ src/Components/Selection/Selection.js
@@ -1,29 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import Movie from '../Movie/Movie'
-import styles from'./Selection.module.css'
+import styles from './Selection.module.css'
 
 export default function Selection() {
     const [movies, setMovies] = useState([])
-    useEffect(() => {
-        fetch("http://www.omdbapi.com/?s=pirate&apikey=d579f432")
+    const [pagination, setPagination] = useState(0)
+
+    const fetchMovies = (pagination) => {
+        fetch(`http://www.omdbapi.com/?s=pirate&apikey=d579f432${pagination !== 0 ? `&page=${pagination}` : ''}`)
             .then(response => response.json())
             .then(data => {
                 if (data.Response === "True") {
-                    console.log("Okay")
+                    setPagination(pagination + 1)
                 }
-                setMovies(data.Search)
+                setMovies([...movies, ...data.Search])
             })
+    }
+
+    useEffect(() => {
+        fetchMovies(pagination)
     }, [])
 
     return (
-        <div className={styles.selection}>
-            {movies.map((movie) => (
-                <Movie
-                    title={ movie.Title }
-                    poster={ movie.Poster}
-                    key={ movie.imdbID}
-                />
-            ))}
+        <div>
+            <div className={styles.selection}>
+                {movies.map((movie) => (
+                    <Movie
+                        title={movie.Title}
+                        poster={movie.Poster}
+                        key={movie.imdbID}
+                    />
+                ))}
+            </div>
+            <button onClick={() => fetchMovies(pagination)}>Load more</button>
         </div>
     )
 }
